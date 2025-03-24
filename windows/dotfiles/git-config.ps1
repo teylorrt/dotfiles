20e999bf99604da7cf1fdb34ef0cb94040f0601a
Teylor Tavares <teylorrt@gmail.com>
2025-03-24 15:09:18 -0300

    Add git config script

--- windows/dotfiles/git-config.ps1
+++ windows/dotfiles/git-config.ps1
@@ -1,12 +1,14 @@
+$name = Read-Host "Enter the name"
+$email = Read-Host "Enter the e-mail"
+
 $isGlobal = Read-Host "Do you want to configure global? (y/n)"
 
 if($isGlobal -eq "y") {
-
-    $name = Read-Host "Enter the name"
     git config --global user.name $name
-
-    $email = Read-Host "Enter the e-mail"
     git config --global user.email $email
-    
     git config --global --add --bool push.autoSetupRemote true
+} else {
+    git config user.name $name
+    git config user.email $email
+    git config --add --bool push.autoSetupRemote true
 }
