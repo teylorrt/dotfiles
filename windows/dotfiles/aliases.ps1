68312a76a1a350f4ad8415d6a3981cb1ad58b023
Teylor Tavares <teylorrt@gmail.com>
2025-04-02 13:18:35 -0300

    Allow alias with parameter

--- windows/dotfiles/aliases.ps1
+++ windows/dotfiles/aliases.ps1
@@ -1,13 +1,32 @@
-### use this file to set your aliases. ###
+### This file contains a collection of aliases for various commands and functions ###
+# It is designed to be used in a PowerShell environment, specifically for Windows.
+# The aliases are defined using the `setAlias` function, which is a custom function defined in commands.ps1
+# The aliases are intended to simplify command usage and improve productivity by providing shorter or more convenient alternatives to commonly used commands.
+# For commands with parameters, the `-p` option is used to indicate that the command requires a parameter.
 ## Note: you can variables defined in the environment.ps1 file
 # example: 
 # setAlias "gis" "git status"
+# setAlias "dev" "cd $DEV"
+# setAlias "gic" "git checkout" "-p" -> alias with parameter. usage: gic <branch_name>
 
 # git aliases
 setAlias "gis" "git status"
 setAlias "gip" "git pull"
-setAlias "gipu" "git push"
-setAlias "main" "git checkout main"
+setAlias "gipp" "git push"
+setAlias "gippf" "git push --force"
+setAlias "gic" "git checkout" "-p"
+setAlias "main" "gic main"
+setAlias "master" "gic master"
+setAlias "girr" "git reset --hard"
+setAlias "giaa" "git add --all"
+setAlias "gicm" "gitCommitAndPush" "-p" # gitCommitAndPush is a custom function defined in commands.ps1
+setAlias "gican" "git commit --amend --no-edit"
+setAlias "gica" "git commit --amend"
+setAlias "gif" "git fetch"
+
+# yarn
+setAlias "yas" "yarn start"
+setAlias "yaf" "yarn format --end-of-line crlf"
 
 #dev folder
 setAlias "dev" "cd $DEV"
--- windows/dotfiles/commands.ps1
+++ windows/dotfiles/commands.ps1
@@ -14,14 +14,42 @@ function global:invokeExpression {
 }
 
 function global:setAlias { 
-    param([String]$alias, [String]$command)
+    param (
+        [Parameter(Mandatory=$true)]
+        [String]$alias, 
+        [Parameter(Mandatory=$true)]
+        [String]$command,
+        [String]$options
+    )
 
+    if($options -eq "-p") {
+        $parameter = 'param ([Parameter(Mandatory=$true)][String]$parameter) _COMMAND_ $parameter'
+        $command = $parameter.Replace("_COMMAND_", $command);
+    }
+    
     $funcName = "$alias";
     $func = 'function global:_NAME_ { _COMMAND_ }'
     $func = $func.Replace("_NAME_", $funcName)
     $func = $func.Replace("_COMMAND_", $command)
 
     invokeExpression($func);
+}
+
+function global:gitCommitAndPush {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$message
+    )
+
+    # Check if the current directory is a Git repository
+    if (-not (Test-Path ".git")) {
+        Write-Host "Error: Current directory is not a Git repository." -ForegroundColor Red
+        return
+    }
+
+    git add -A
+    git commit -m $message
+    git push
 }
 
 $global:commandsLoaded = $true
